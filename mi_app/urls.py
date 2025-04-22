1a91ba2cce959f2afa06453f5b1a3d876fb533e7
jazielSlayer <jazielarmandovargaschoque.com>
2025-04-22 16:28:48 -0400

    Reinicio

--- mi_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.hola_mundo, name='hola_mundo'),
-    path('registro/estudiante/', views.registro_estudiante, name='registro_estudiante'),
-    path('registro/docente/', views.registro_docente, name='registro_docente'),
-]
